b273bf8461cd31146b261a87b055ee1b6ca92904
Miguelmargar <miguelitalia@hotmail.com>
2020-02-12 20:02:34 +0000

    added file extension checking to ensure jpg and png files only accepted otherwise alert user

--- static/photo.js
+++ static/photo.js
@@ -1,11 +1,18 @@
-$("#myFile").change(function() {
-	var file = $("#myFile")[0].files[0];
-	var reader = new FileReader();
-
-	reader.onload = function (e) {
-    	$('.currImg img')
-        	.attr('src', e.target.result)
-            };
-            reader.readAsDataURL(file);
+$("#myFile").change(function(e) {
+	var fileName = e.target.files[0].name;
+	var fileExt = fileName.slice(-3);
 	
+	if (fileExt == "jpg" || fileExt == "png") {
+		var file = $("#myFile")[0].files[0];
+		var reader = new FileReader();
+
+		reader.onload = function (e) {
+			$('.currImg img').attr('src', e.target.result);
+		};
+		reader.readAsDataURL(file);
+	} else {
+		alert("File extension not accepted - jpg and png file needed.")
+	}
 })
+
+
